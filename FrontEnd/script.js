4ffa516c499bc8b2c675c3e27da8cc14939177d5
Camille Mallet <mallet.camille11@gmail.com>
2023-09-12 17:10:10 +0200

    feat(Js): add button filters by categories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,21 @@
+// On récupère les projets depuis le Back-end //
 async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     const works = await response.json();
     return works;
 }
 
+// On récupère les catégories de projet depuis le Back-end //
+async function getCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const categories = await response.json();
+    return categories;
+}
 
+// On appelle les fonctions au chargement du body dans le DOM //
 document.body.onload = function () {
     createProjects();
+    createFilters();
 }
 
 async function createProjects() {
@@ -28,4 +37,16 @@ async function createProjects() {
     } 
 }
 
+async function createFilters () {
+    const categories = await getCategories();
+    let nbCategories = categories.length // On compte le nombre d'éléments dans le tableau categories //
+    for (let i=0; i<nbCategories; i++) { 
+        // On crée des boutons qui s'appuient sur les catégories que j'ai récupérées du Back-end //
+        const buttonFilters = document.createElement("button");
+        buttonFilters.classList.add('button__filters');
+        buttonFilters.innerHTML=categories[i].name;
+        const containerButtons = document.querySelector(".buttons__container");
+        containerButtons.appendChild(buttonFilters);
+    }
+}
 
