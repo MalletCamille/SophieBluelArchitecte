36b0412a01b501116eb57b358413fda2d124ba62
Camille Mallet <mallet.camille11@gmail.com>
2023-09-20 16:09:23 +0200

    feat(login.js): post request setup

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,8 +1,22 @@
+async function postUsersLogin() {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: 'POST',
+        body: {
+            "email": "string",
+            "password": "string"
+        }
+    })
+}
+
+
+
+
 document.body.onload = function () {
 
     let loginForm = document.querySelector(".container__login form")    
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault ()
         console.log("le formulaire est envoyé")
+        console.log(event)
     })
 }
