aa3cc4cc5f4602f4c6694292ffb11a76796be4d3
Camille Mallet <mallet.camille11@gmail.com>
2023-09-22 16:02:14 +0200

    test(login.js) : wip login request

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,16 +1,26 @@
+
+ // Envoi du login et mdp saisis par l'utilisateur au Back via la méthode Post //
 async function postUsersLogin() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log('Nom d\'utilisateur :', email);
+    console.log('Mot de passe :', password);
     const response = await fetch("http://localhost:5678/api/users/login", {
         method: 'POST',
+        headers:{
+            'Content-Type' :'application/json'
+        },
         body: {
-            "email": "string",
-            "password": "string"
+            "email": email,
+            "password": password,
         }
     })
+    console.log(await response);
 }
 
 
 
-
+ // Fonction Submit pour récupérer les informations de login et mdp saisies par l'utilisateur //
 document.body.onload = function () {
 
     let loginForm = document.querySelector(".container__login form")    
@@ -18,5 +28,6 @@ document.body.onload = function () {
         event.preventDefault ()
         console.log("le formulaire est envoyé")
         console.log(event)
+        postUsersLogin();
     })
 }
